0d3b7710d196af85f5905c51f75aa4fd360e25d8
Edouard Misset <edouardmisset@gmail.com>
2021-10-21 18:54:51 +0200

    implemented the fetch function to get data from the ‘server'

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,7 @@
 import { User } from './User'
 
-const user = new User({ name: 'James', age: 20 })
+const user = new User({ id: 1 })
 
-user.on('change', () => {
-  console.log('change 1')
-})
-user.on('change', () => {
-  console.log('change 1')
-})
-user.on('save', () => {
-  console.log('saved was trigger')
-})
+user.fetch()
 
-user.trigger('save')
+console.log(user)
